a5fdc092dfacbba32cc65db9db0015f1167bb3a7
xanzinfl <andersonjvazquez@aol.com>
2025-08-06 21:03:11 -0400

    Rm Stoner Tools

--- Stoner tools/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-### 1.0.3 
-- Fixed the widgets randomly changings sizes.
-
-### 1.0.2
-- Made the widgets look alot cleaner with a translucent background and rounded corners.
--- Stoner tools/widgets/ripcounter/client.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const ripCounterEl = document.getElementById('rip-counter');
-    const socket = io();
-
-    // Listen for the 'rip-update' event
-    socket.on('rip-update', (count) => {
-        ripCounterEl.textContent = count;
-    });
-});
